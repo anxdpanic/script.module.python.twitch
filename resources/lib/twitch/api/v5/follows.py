2a4e40dab9169ddbd1f85d2475b21a87ea9992d4
Tim <anxdpanic@users.noreply.github.com>
2017-03-15 16:43:47 -0400

    update follow/unfollow

--- resources/lib/twitch/api/v5/follows.py
+++ resources/lib/twitch/api/v5/follows.py
@@ -5,6 +5,7 @@ from twitch import keys
 from twitch.api.parameters import Direction, SortBy
 from twitch.queries import V5Query as Qry
 from twitch.queries import query
+from twitch.api.parameters import Boolean
 
 
 @query
@@ -30,24 +31,30 @@ def by_id(identification, limit=25, offset=0, direction=Direction.DESC,
 
 
 @query
-def status(user, target):
-    q = Qry('users/{user}/follows/channels/{target}')
-    q.add_urlkw(keys.USER, user)
+def status(identification, target):
+    q = Qry('users/{id}/follows/channels/{target}')
+    q.add_urlkw(keys.ID, identification)
     q.add_urlkw(keys.TARGET, target)
     return q
 
 
 # Needs Auth, needs PUT
 @query
-def follow(user, target):
-    raise NotImplementedError
+def follow(identification, target, notification=Boolean.FALSE):
+    q = Qry('users/{id}/follows/channels/{target}', method='PUT')
+    q.add_urlkw(keys.ID, identification)
+    q.add_urlkw(keys.TARGET, target)
+    q.add_data('notification', notification)
+    return q
 
 
 # Needs Auth, needs DELETE
 @query
-def unfollow(user, target):
-    raise NotImplementedError
-
+def unfollow(identification, target):
+    q = Qry('users/{id}/follows/channels/{target}', method='DELETE')
+    q.add_urlkw(keys.ID, identification)
+    q.add_urlkw(keys.TARGET, target)
+    return q
 
 # Needs Auth
 @query
