df559559c1949b4607de257d8ae9ece852aa09b7
anxdpanic <anxdpanic@users.noreply.github.com>
2017-03-28 13:08:44 -0400

    [v5] update chat, descriptive function naming

--- resources/lib/twitch/api/v5/chat.py
+++ resources/lib/twitch/api/v5/chat.py
@@ -7,20 +7,20 @@ from twitch.queries import query
 
 
 @query
-def emoticons_by_set(emotesets=list()):
+def get_emoticons_by_set(emotesets=None):
     q = Qry('chat/emoticon_images')
-    q.add_param(keys.EMOTESETS, emotesets, list())
+    q.add_param(keys.EMOTESETS, emotesets, None)
     return q
 
 
 @query
-def badges(name):
-    q = Qry('chat/{channel}/badges')
-    q.add_urlkw(keys.CHANNEL, name)
+def get_badges_by_channel(channel_id):
+    q = Qry('chat/{channel_id}/badges')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
     return q
 
 
 @query
-def emoticons():
+def get_all_emoticons():
     q = Qry('chat/emoticons')
     return q
