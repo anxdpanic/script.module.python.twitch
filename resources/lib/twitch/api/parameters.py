3df71115e6858504f3cd7084afaca25abc675c7a
anxdpanic <anxdpanic@users.noreply.github.com>
2017-03-28 13:03:47 -0400

    [v5] add missing end-points to channels, descriptive function naming

--- resources/lib/twitch/api/parameters.py
+++ resources/lib/twitch/api/parameters.py
@@ -41,6 +41,13 @@ class SortBy(_Parameter):
     _valid = [CREATED_AT, LAST_BROADCAST, LOGIN]
 
 
+class VideoSort(_Parameter):
+    VIEWS = 'views'
+    TIME = 'time'
+
+    _valid = [VIEWS, TIME]
+
+
 class BroadcastType(_Parameter):
     ARCHIVE = 'archive'
     HIGHLIGHT = 'highlight'
@@ -73,3 +80,21 @@ class Cursor(_Parameter):
             return value
         except ValueError:
             raise ValueError(value)
+
+
+class Language(_Parameter):
+    ALL = 'all'
+    # add twitch supported language codes
+    _valid = [ALL]
+
+    @classmethod
+    def validate(cls, value):
+        split_values = value.split(',')
+        for val in split_values:
+            if val not in cls._valid:
+                raise ValueError(value)
+        return value
+
+
+class Duration(_Parameter):
+    _valid = [30, 60, 90, 120, 150, 180]
--- resources/lib/twitch/api/v5/channels.py
+++ resources/lib/twitch/api/v5/channels.py
@@ -1,58 +1,72 @@
 # -*- encoding: utf-8 -*-
 # https://dev.twitch.tv/docs/v5/reference/channels/
 
-from twitch import keys
+from twitch import keys, methods
 from twitch.queries import V5Query as Qry
 from twitch.queries import query
-from twitch.api.parameters import Boolean, BroadcastType, Direction
+from twitch.api.parameters import Boolean, BroadcastType, Cursor, Direction, Duration, Language, VideoSort
 
 
 @query
-def by_id(channel_id):
-    q = Qry('channels/{id}')
-    q.add_urlkw(keys.ID, channel_id)
+def get_channel():
+    q = Qry('channels')
     return q
 
 
 @query
-def channel():
-    raise NotImplementedError
-
-
-@query
-def editors(name):
-    raise NotImplementedError
-
-
-# TODO needs Authentication and put requests
-@query
-def update(name, status=None, game=None, delay=0):
-    raise NotImplementedError
-
-
-# TODO needs auth
-@query
-def delete(name):
-    raise NotImplementedError
-
-
-# TODO needs auth, needs POST request
-@query
-def commercial(name, length=30):
-    raise NotImplementedError
-
-
-@query
-def teams(channel_id):
-    q = Qry('channels/{id}/teams')
-    q.add_urlkw(keys.ID, channel_id)
+def get_channel_by_id(channel_id):
+    q = Qry('channels/{channel_id}')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
     return q
 
 
 @query
-def followers(channel_id, limit=25, offset=0, direction=Direction.DESC):
-    q = Qry('channels/{id}/follows')
-    q.add_urlkw(keys.ID, channel_id)
+def update_channel(channel_id, status=None, game=None, delay=None, channel_feed_enabled=None):
+    q = Qry('channels/{channel_id}', method=methods.PUT)
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    channel = {}
+    if status is not None:
+        channel[keys.STATUS] = status
+    if game is not None:
+        channel[keys.GAME] = game
+    if delay is not None:
+        channel[keys.DELAY] = delay
+    if channel_feed_enabled is not None:
+        channel[keys.CHANNEL_FEED_ENABLED] = Boolean.validate(channel_feed_enabled)
+    q.add_data(keys.CHANNEL, channel)
+    return q
+
+
+@query
+def get_channel_editors(channel_id):
+    q = Qry('channels/{channel_id}/editors')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    return q
+
+
+@query
+def get_channel_followers(channel_id, limit=25, offset=0, cursor='MA==', direction=Direction.DESC):
+    q = Qry('channels/{channel_id}/follows')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    q.add_param(keys.LIMIT, limit, 25)
+    q.add_param(keys.OFFSET, offset, 0)
+    q.add_param(keys.CURSOR, Cursor.validate(cursor), 'MA==')
+    q.add_param(keys.DIRECTION, direction, Direction.DESC)
+    return q
+
+
+@query
+def get_channel_teams(channel_id):
+    q = Qry('channels/{channel_id}/teams')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    return q
+
+
+# Needs Authentication
+@query
+def get_channel_subscribers(channel_id, limit=25, offset=0, direction=Direction.ASC):
+    q = Qry('channels/{channel_id}/subscriptions')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
     q.add_param(keys.LIMIT, limit, 25)
     q.add_param(keys.OFFSET, offset, 0)
     q.add_param(keys.DIRECTION, direction, Direction.DESC)
@@ -61,23 +75,61 @@ def followers(channel_id, limit=25, offset=0, direction=Direction.DESC):
 
 # Needs Authentication
 @query
-def subscribers(channel_id, limit=25, offset=0, direction=Direction.ASC):
-    raise NotImplementedError
-
-
-# Needs Authentication
-@query
-def subscription_status(channel_id, user_id):
-    raise NotImplementedError
+def check_channel_subscription(channel_id, user_id):
+    q = Qry('channels/{channel_id}/subscriptions/{user_id}')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    q.add_urlkw(keys.USER_ID, user_id)
+    return q
 
 
 @query
-def videos(channel_id, limit=10, offset=0,
-           broadcast_type=BroadcastType.HIGHLIGHT, hls=Boolean.FALSE):
+def get_channel_videos(channel_id, limit=10, offset=0,
+                       broadcast_type=BroadcastType.HIGHLIGHT,
+                       hls=Boolean.FALSE, sort_by=VideoSort.TIME,
+                       language=Language.ALL):
     q = Qry('channels/{id}/videos')
     q.add_urlkw(keys.ID, channel_id)
     q.add_param(keys.LIMIT, limit, 10)
     q.add_param(keys.OFFSET, offset, 0)
     q.add_param(keys.BROADCAST_TYPE, BroadcastType.validate(broadcast_type))
+    q.add_param(keys.SORT, VideoSort.validate(sort_by), VideoSort.TIME)
+    q.add_param(keys.LANGUAGE, Language.validate(language), Language.ALL)
     q.add_param(keys.HLS, Boolean.validate(hls), Boolean.FALSE)
+    return q
+
+
+@query
+def start_commercial(channel_id, duration=30):
+    q = Qry('channels/{channel_id}/commercial', method=methods.POST)
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    q.add_data(keys.DURATION, Duration.validate(duration))
+    return q
+
+
+@query
+def reset_stream_key(channel_id):
+    q = Qry('channels/{channel_id}/stream_key', method=methods.DELETE)
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    return q
+
+
+@query
+def get_channel_community(channel_id):
+    q = Qry('channels/{channel_id}/community')
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    return q
+
+
+@query
+def set_channel_community(channel_id, community_id):
+    q = Qry('channels/{channel_id}/community/{community_id}', method=methods.PUT)
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
+    q.add_urlkw(keys.COMMUNITY_ID, community_id)
+    return q
+
+
+@query
+def delete_channel_from_community(channel_id):
+    q = Qry('channels/{channel_id}/community', method=methods.DELETE)
+    q.add_urlkw(keys.CHANNEL_ID, channel_id)
     return q
