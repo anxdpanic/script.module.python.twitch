09ae781f69e70aa43988dcd542190c5cec9683c1
anxdpanic <anxdpanic@users.noreply.github.com>
2017-03-28 14:10:28 -0400

    [v5] update streams, descriptive function naming

--- resources/lib/twitch/api/v5/streams.py
+++ resources/lib/twitch/api/v5/streams.py
@@ -4,11 +4,11 @@
 from twitch import keys
 from twitch.queries import V5Query as Qry
 from twitch.queries import query
-from twitch.api.parameters import StreamType
+from twitch.api.parameters import StreamType, Language
 
 
 @query
-def by_id(channel_id, stream_type=StreamType.LIVE):
+def get_streams_by_user(channel_id, stream_type=StreamType.LIVE):
     q = Qry('streams/{id}')
     q.add_urlkw(keys.ID, channel_id)
     q.add_param(keys.STREAM_TYPE, StreamType.validate(stream_type), StreamType.LIVE)
@@ -16,36 +16,37 @@ def by_id(channel_id, stream_type=StreamType.LIVE):
 
 
 @query
-def all(game=None, channel_ids=None, community_id=None, language=None, limit=25, offset=0, client_id=None):
+def get_streams(game=None, channel_ids=None, community_id=None, language=Language.ALL,
+                stream_type=StreamType.LIVE, limit=25, offset=0):
     q = Qry('streams')
     q.add_param(keys.GAME, game)
     q.add_param(keys.CHANNEL, channel_ids)
     q.add_param(keys.COMMUNITY_ID, community_id)
-    q.add_param(keys.LANGUAGE, language)
+    q.add_param(keys.LANGUAGE, Language.validate(language), Language.ALL)
+    q.add_param(keys.STREAM_TYPE, StreamType.validate(stream_type), StreamType.LIVE)
     q.add_param(keys.LIMIT, limit, 25)
     q.add_param(keys.OFFSET, offset, 0)
-    q.add_param(keys.CLIENT_ID, client_id)
     return q
 
 
 @query
-def featured(limit=25, offset=0):
+def get_streams_summary(game=None):
+    q = Qry('streams/summary')
+    q.add_param(keys.GAME, game)
+    return q
+
+
+@query
+def get_featured_streams(limit=25, offset=0):
     q = Qry('streams/featured')
     q.add_param(keys.LIMIT, limit, 25)
     q.add_param(keys.OFFSET, offset, 0)
     return q
 
 
-@query
-def summary(game=None):
-    q = Qry('streams/summary')
-    q.add_param(keys.GAME, game)
-    return q
-
-
 # Needs Authentication
 @query
-def followed(stream_type=StreamType.LIVE, limit=25, offset=0):
+def get_followed_streams(stream_type=StreamType.LIVE, limit=25, offset=0):
     q = Qry('streams/followed')
     q.add_param(keys.STREAM_TYPE, StreamType.validate(stream_type), StreamType.LIVE)
     q.add_param(keys.LIMIT, limit, 25)
