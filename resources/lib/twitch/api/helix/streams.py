d38fcf05fedd880f3c5021a8d6eb32967b4f01b5
anxdpanic <anxdpanic@users.noreply.github.com>
2017-12-19 14:20:55 -0500

    add helix clips, games,videos and update streams

--- resources/lib/twitch/api/helix/streams.py
+++ resources/lib/twitch/api/helix/streams.py
@@ -2,7 +2,7 @@
 # https://dev.twitch.tv/docs/api/reference
 
 from twitch import keys
-from twitch.api.parameters import Cursor, Language, StreamTypes
+from twitch.api.parameters import Cursor, Language, StreamTypeHelix
 from twitch.queries import HelixQuery as Qry
 from twitch.queries import query
 
@@ -10,16 +10,17 @@ from twitch.queries import query
 # required scope: none
 @query
 def get_streams(community_id=list(), game_id=list(), user_id=list(),
-                user_login=list(), stream_type=StreamTypes.ALL, language=list(),
-                after='MA==', first=20, use_app_token=False):
+                user_login=list(), stream_type=StreamTypeHelix.ALL, language=list(),
+                after='MA==', before='MA==', first=20, use_app_token=False):
     q = Qry('streams', use_app_token=use_app_token)
     q.add_param(keys.AFTER, Cursor.validate(after), 'MA==')
+    q.add_param(keys.BEFORE, Cursor.validate(before), 'MA==')
     q.add_param(keys.FIRST, first, 20)
     q.add_param(keys.COMMUNITY_ID, community_id, list())
     q.add_param(keys.GAME_ID, game_id, list())
     q.add_param(keys.USER_ID, user_id, list())
     q.add_param(keys.USER_LOGIN, user_login, list())
-    q.add_param(keys.TYPE, StreamTypes.validate(stream_type), StreamTypes.ALL)
+    q.add_param(keys.TYPE, StreamTypeHelix.validate(stream_type), StreamTypeHelix.ALL)
     if isinstance(language, list):
         _language = [lang for lang in language if lang in Language.valid()]
         q.add_param(keys.LANGUAGE, _language, list())
@@ -32,16 +33,17 @@ def get_streams(community_id=list(), game_id=list(), user_id=list(),
 # required scope: none
 @query
 def get_metadata(community_id=list(), game_id=list(), user_id=list(),
-                 user_login=list(), stream_type=StreamTypes.ALL, language=list(),
-                 after='MA==', first=20, use_app_token=False):
+                 user_login=list(), stream_type=StreamTypeHelix.ALL, language=list(),
+                 after='MA==', before='MA==', first=20, use_app_token=False):
     q = Qry('streams/metadata', use_app_token=use_app_token)
     q.add_param(keys.AFTER, Cursor.validate(after), 'MA==')
+    q.add_param(keys.BEFORE, Cursor.validate(before), 'MA==')
     q.add_param(keys.FIRST, first, 20)
     q.add_param(keys.COMMUNITY_ID, community_id, list())
     q.add_param(keys.GAME_ID, game_id, list())
     q.add_param(keys.USER_ID, user_id, list())
     q.add_param(keys.USER_LOGIN, user_login, list())
-    q.add_param(keys.TYPE, StreamTypes.validate(stream_type), StreamTypes.ALL)
+    q.add_param(keys.TYPE, StreamTypeHelix.validate(stream_type), StreamTypeHelix.ALL)
     if isinstance(language, list):
         _language = [lang for lang in language if lang in Language.valid()]
         q.add_param(keys.LANGUAGE, _language, list())
